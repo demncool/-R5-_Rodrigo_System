8fc40e358e4527e9328e3b344f97c78f96a60a7f
demncool <bryanjamesbjr06@gmail.com>
2020-10-22 21:55:31 +0800

    Final

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,42 @@ $router->get('/', function () use ($router) {
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('/users', ['uses' => 'UserController@getUsers']);
 });
+
+$router->get('login', [
+    'as' => 'login', 'uses' => 'UserController@loginPage'
+]);
+
+//log in button
+$router->post('verify', [
+    'as' => 'verify', 'uses' => 'UserController@verifyUser'
+]);
+
+//add user page
+$router->post('addUser', [
+    'as' => 'addUser', 'uses' => 'UserController@newUserPage'
+]);
+
+//add user button
+$router->post('insert', [
+    'as' => 'insert', 'uses' => 'UserController@insertUser'
+]);
+
+//delete user page
+$router->get('delUserPage', [
+    'as' => 'delUserPage', 'uses' => 'UserController@deleteUserPage'
+]);
+
+//delete user button
+$router->post('deleteUser', [
+    'as' => 'deleteUser', 'uses' => 'UserController@delete'
+]);
+
+//update user page
+$router->post('updateUserPage', [
+    'as' => 'updateUserPage', 'uses' => 'UserController@updateUserPage'
+]);
+
+//update user button
+$router->post('updateUser', [
+    'as' => 'updateUser', 'uses' => 'UserController@update'
+]);
